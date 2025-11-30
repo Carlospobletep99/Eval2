7bc061aaaa85cffbf08b5bd1bf8be576e42440fd
Carlospobletep99 <carlo.pobletep@duocuc.cl>
2025-11-29 23:45:33 -0300

    ip instancia ec2

--- app/src/main/java/com/example/eval2/network/RetrofitClient.kt
+++ app/src/main/java/com/example/eval2/network/RetrofitClient.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // mi IP local (Carlos):
-    private const val BASE_URL = "http://192.168.1.9:8080/"
+    // IP INSTANCIA EC2:
+    private const val BASE_URL = "http://35.171.52.41:8080/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
